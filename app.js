4e3539b7ac8be4235f81b2c8e73da5957c070fd0
Ajay <ajayvishwakarma457@gmail.com>
2025-04-21 18:01:19 +0530

    changes

---
+++ app.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Mac!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}`);
+});
